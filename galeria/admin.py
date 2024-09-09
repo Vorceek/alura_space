9dedace133bfbcf468f29da53c7d2e33780bab54
Vinicios <vinicios.mensen@domeconsultoria.com.br>
2024-09-09 17:24:00 -0300

    commit 3

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda") 
-    list_display_links = ("id", "nome")
-    search_fields = ("nome",)
+    list_display = ("id", "nome", "legenda") #O que aparece no display admin
+    list_display_links = ("id", "nome") #Links no nome "ID" e "Nome"
+    search_fields = ("nome",) #Campo de Pesquisa
+    list_filter = ("categoria",) #Filtros da direita
+    list_per_page = 10 #O máximo de conteúdo por página admin
 
 admin.site.register(Fotografia, ListandoFotografias)
