f40d9a726d517c9028e29b79153d6467224cfa5d
Vinicios <vinicios.mensen@domeconsultoria.com.br>
2024-09-11 17:23:18 -0300

    commit 4

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda") #O que aparece no display admin
+    list_display = ("id", "nome", "legenda", "publicada") #O que aparece no display admin
     list_display_links = ("id", "nome") #Links no nome "ID" e "Nome"
     search_fields = ("nome",) #Campo de Pesquisa
     list_filter = ("categoria",) #Filtros da direita
+    list_editable = ("publicada",) #Poder editar no admin
     list_per_page = 10 #O máximo de conteúdo por página admin
 
 admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 class Fotografia(models.Model):
@@ -13,7 +14,10 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=100, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default="")
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d", blank=True)
+    data_fotografia = models.DateTimeField(default=datetime.now , blank=False)
+    publicada = models.BooleanField(default=False)
+ 
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
